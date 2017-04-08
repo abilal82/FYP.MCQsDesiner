cc35c9bf5ee39b9bbb441a7df888a9bc71a7d194
bilal82 <m.bilalaslam82@gmail.com>
2017-04-08 17:33:14 +0500

    AddedStudentClass

--- MCQsDesigner.BOL/Properties/AssemblyInfo.cs
+++ MCQsDesigner.BOL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCQsDesigner.UI")]
+[assembly: AssemblyTitle("MCQsDesigner.BOL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MCQsDesigner.UI")]
+[assembly: AssemblyProduct("MCQsDesigner.BOL")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f437edfd-3d0d-40a8-aa8b-ed9f2418bc95")]
+[assembly: Guid("89c9f2ab-b203-4d43-be14-e8c334360ec4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
